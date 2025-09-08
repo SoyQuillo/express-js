200f1cdbf299e84263a57dd5fd7f8c66904522fc
SoyQuillo <santiagojose190305@gmail.com>
2025-09-08 16:49:41 -0500

    introduccion a middlewares

--- index.js
+++ index.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.all('/info', (req, res) =>{
-    res.send('server info')
+app.use((req, res, next) =>{
+    console.log(`Ruta: ${req.url} metodo: ${req.method} `)
+    next()
 })
+
+
+app.get("/profile", (req, res) => {
+
+    res.send('profile page')
+
+});
+
+app.all("/about", (req, res) => {
+
+    res.send('about page')
+
+});
 
 app.listen(3000);
 console.log(` Server on port ${3000}`);
