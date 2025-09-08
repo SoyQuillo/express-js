30532b0e0b527ef26d64ff5689a98c053f838485
SoyQuillo <santiagojose190305@gmail.com>
2025-09-08 14:25:18 -0500

    distintas maneras del usuario enviar datos al servidor

--- index.js
+++ index.js
@@ -2,32 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hola mundo");
-});
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({extended:false})); //entender los datos de un formulario
 
-app.get("/miarchivo", (req, res) => {
-  res.sendFile("./javascript.png", {
-    root: __dirname,
-  });
-});
-
-app.get("/user", (req, res) => {
-  res.json({
-    name: "santiago",
-    lastname: "quintero",
-    edad: "40",
-    points: [1, 2, 3],
-    address: {
-      city: "new york",
-      street: "some steet 123",
-    },
-  });
-});
-
-app.get("/isAlive", (req, res) => {
-  res.sendStatus(204);
-});
+app.post('/user', (req, res ) =>{
+    console.log(req.body);
+    res.send("Nuevo usuario creado")
+})
 
 app.listen(3000);
 console.log(` Server on port ${3000}`);
